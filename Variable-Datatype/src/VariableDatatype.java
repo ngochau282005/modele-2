1331026ebb1b1561d988d0b98173088519e10ccc
ngochau282005 <ngochau24042022@gmail.com>
2023-11-03 14:23:15 +0700

    add file

---
+++ Variable-Datatype/src/VariableDatatype.java
@@ -0,0 +1,17 @@
+public class VariableDatatype {
+    public static void main(String[]args){
+        int i = 10;
+        System.out.println("i = " + i);
+        float f = 25.5f;
+        System.out.println("f = "+ f);
+        double d = 25.5;
+        System.out.println("d = " + d);
+        boolean b = true;
+        System.out.println("b = " + b);
+        char c = 'a';
+        System.out.println("c = " + c);
+        String s = "Hà Nội";
+        System.out.println("s = " + s);
+    }
+}
+
